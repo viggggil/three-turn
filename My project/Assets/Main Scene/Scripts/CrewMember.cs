4c4f522e4b71f48fdf0644b569dba7f6dc9e41fb
FayYu <1005351271@qq.com>
2024-12-31 15:37:49 +0800

    一些初始化
    更改了一下文件夹结构，增加了几个玩家的贴图（试用），加了一个新的C#文件

---
+++ My project/Assets/Main Scene/Scripts/CrewMember.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrewMember : MonoBehaviour
+{
+   [SerializeField] int PlayerHP;
+
+   private bool haveAttacked;//判断该队员是否已经做出了行动
+
+}
