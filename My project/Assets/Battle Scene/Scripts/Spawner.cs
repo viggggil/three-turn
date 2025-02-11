e59e24c0a84edf1fd65f98a2a0ebe99bf07b4dbf
FayYu <1005351271@qq.com>
2025-02-11 22:39:22 +0800

    人物传入、字库和按钮
    基本完成人物的传入，创建了一个汉字TMP资产，现在textmeshpro可以输入汉字，正在尝试制作战斗场景按钮

--- My project/Assets/Battle Scene/Scripts/Spawner.cs
+++ My project/Assets/Battle Scene/Scripts/Spawner.cs
@@ -9,19 +9,25 @@ public class Spawner : MonoBehaviour
    [SerializeField] GameObject Player2;
    [SerializeField] GameObject Player3;
 
+   [Header("Transforms")]
+   public Transform transform1;
+   public Transform transform2;
+   public Transform transform3;
+
    public List<GameObject> spawnList;
    public int spawnCount = 0;
 
    public List<GameObject> nodeList;
     
-   private int position1 = 0;
+   private int position1 = 0;//测试阶段，先令定值
 
    public void LoadPlayers()
    {
       Player1 = spawnList[0];
-      Player2 = spawnList[1];
-      Player3 = spawnList[2];
-      Instantiate(Player1, nodeList[position1].transform);
+      //Player2 = spawnList[1];
+      //Player3 = spawnList[2];
+      transform1 = nodeList[0].transform;
+      Instantiate(Player1, transform1);
 
       //在此补充进入场景前载入成员的逻辑
       //扫描成员、使其成为某个node下面的子对象，并将成员加入列表，方便后面比较速度决定行动顺序
